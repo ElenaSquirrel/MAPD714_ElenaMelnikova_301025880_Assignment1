c7aef8a280ac67fb9afa48f67c074c27e5f36b21
Elena Melnikova <elenamelnikova@owners-macbook-pro.local>
2018-09-27 09:46:16 -0400

    Base methods for field of buttons and operations added; number keys (0 to 9) set as normalize functions

--- MAPD714_ElenaMelnikova_301025880/ViewController.swift
+++ MAPD714_ElenaMelnikova_301025880/ViewController.swift
@@ -13,16 +13,16 @@ class ViewController: UIViewController {
 
     
     
-    var screenNumber:Double? = nil;
+    var screenNumber:Double? = nil
     
 
-    var previousNumber:Double? = nil;
+    var previousNumber:Double? = nil
     
   
-    var operation:Int? = nil;
+    var operation:Int? = nil
     
  
-    var performingMath = false;
+    var performingMath = false
     
 
     @IBOutlet weak var label:UILabelPadding! //UILabel! with paddings that set in UILabelPaddind.swift
@@ -30,7 +30,7 @@ class ViewController: UIViewController {
     func normalize(input:String) -> String {
      
         let isMinus = String(input.prefix(1))
-        var str: String;
+        var str: String
         
       
         if isMinus == "-" {
@@ -54,6 +54,7 @@ class ViewController: UIViewController {
 
     @IBAction func numberField(_ sender: UIButton) {
 
+     
         
     }
 
