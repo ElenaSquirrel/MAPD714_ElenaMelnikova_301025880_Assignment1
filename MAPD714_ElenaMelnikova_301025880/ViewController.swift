f1572de73766f213983c095b3f73ec8046b37fd9
Elena Melnikova <elenamelnikova@owners-macbook-pro.local>
2018-09-27 12:33:06 -0400

    Error message; "+/-" operation; combinations: previousNumber is nil and screenNumber is nil; previousNumber is nil and screenNumber is not nil; previousNumber is not nil and screenNumber is nil; previousNumber is not nil and screenNumber is not nil

--- MAPD714_ElenaMelnikova_301025880/ViewController.swift
+++ MAPD714_ElenaMelnikova_301025880/ViewController.swift
@@ -10,73 +10,98 @@ import UIKit
 
 
 class ViewController: UIViewController {
-
-    
     
     var screenNumber:Double? = nil
     
-
     var previousNumber:Double? = nil
     
-  
     var operation:Int? = nil
     
- 
     var performingMath = false
     
-
     @IBOutlet weak var label:UILabelPadding! //UILabel! with paddings that set in UILabelPaddind.swift
     
     func normalize(input:String) -> String {
-     
+        
         let isMinus = String(input.prefix(1))
         var str: String
         
-      
         if isMinus == "-" {
-        
-            str = String(input.prefix(9))
+            
+            str = String(input.prefix(10))
         } else {
-          
+            
             str = String(input.prefix(9))
         }
         
-
         return str
     }
     
-  
     func normalize(input:Double) -> String {
         let str = String(input)
         return normalize(input:str)
     }
     
-
     @IBAction func numberField(_ sender: UIButton) {
-
+        
         if performingMath == true {
-
+            
             performingMath = false
             
             if sender.tag == 0  {
-               
+                
                 label.text = "0."
-
+                
                 screenNumber = Double(label.text!)!
             } else {
                 
-                let str = normalize(input: String(sender.tag - 1))
-        
+                let str = String(sender.tag - 1)
+                
                 screenNumber = Double(str)!
-               
+                
                 label.text = str
+            }
+        } else {
+            
+            if sender.tag == 0  {
+                
+                if label.text! == "" {
+                    
+                    label.text="0"
+                }
+                
+                label.text=label.text!+"."
+                
+                screenNumber = Double(label.text!)!
+            } else
+            {
+                if label.text != "0" && label.text != "Error" {
+                    
+                    var str = String(label.text!+String(sender.tag-1))
+                    
+//                    if String(str.prefix(1)) == "-" {
+//                        str = String(str.prefix(10))
+//                    } else {
+//                        str = String(str.prefix(9))
+//                    }
+                    str = normalize(input: str)
+                    
+                    screenNumber = Double(str)
+                    
+                    label.text=str
+                } else {
+                    
+                    label.text=String(sender.tag-1)
+                    
+                    screenNumber = Double(label.text!)!
+                }
             }
         }
         
     }
-
+    
     @IBAction func operationsField(_ sender: UIButton) {
-
+  
         //"C" clicked
         if sender.tag == 11
         {
@@ -88,13 +113,49 @@ class ViewController: UIViewController {
             performingMath = false;
             return
         }
+
+        if label.text == "Error" {
+            return
+        }
+
+        if sender.tag == 12 {
+            screenNumber = -screenNumber!
+            let str = normalize(input: screenNumber!)
+            label.text = str
+            return
+        }
         
-       
         if label.text != "" && sender.tag != 18
         {
             if performingMath {
-            
+                //Update operation
                 operation = sender.tag
+
+                label.text = operationToLabel(tag: sender.tag)
+                return
+            }
+
+            if previousNumber == nil && screenNumber == nil {
+                return
+            }
+
+            if previousNumber == nil && screenNumber != nil {
+                previousNumber = screenNumber
+                screenNumber = nil
+
+                operation = sender.tag
+ 
+                performingMath = true
+
+                label.text = operationToLabel(tag: sender.tag)
+                return
+            }
+
+            if previousNumber != nil && screenNumber == nil {
+                
+                operation = sender.tag
+ 
+                performingMath = true
 
                 label.text = operationToLabel(tag: sender.tag)
                 return
@@ -103,29 +164,29 @@ class ViewController: UIViewController {
             if previousNumber != nil && screenNumber != nil {
                 
                 if (screenNumber! == 0 && operation! == 14) {
-                    //Division by 0
-                    
-                    //Set error
+
                     previousNumber = nil
                     screenNumber = nil
                     operation = nil
                     label.text = "Error"
                     performingMath = false
                     return
-                } else {
-                   
+                } else
+
+                {
+
                     let res = oper(num1:previousNumber, num2:screenNumber, operation:operation!)
-                
+
                     let str = normalize(input: res)
-             
+   
                     let dbl = Double(str)
-                    
+
                     label.text = str
- 
+
                     previousNumber = dbl
-                    
+
                     screenNumber = nil
- 
+
                     operation = sender.tag
 
                     performingMath = true
@@ -136,16 +197,59 @@ class ViewController: UIViewController {
         }
         else if sender.tag == 18
         {
-            //Operation "="
-
             performingMath = false
-  
+
+            if previousNumber == nil {
+                return
+            }
+
+            if previousNumber != nil && operation==13 {
+
+                if previousNumber! < 0 {
+                    previousNumber = nil
+                    screenNumber = nil
+                    operation = nil
+                    label.text = "Error"
+                    performingMath = false
+                    return
+                }
+
+                let res = oper(num1:previousNumber, num2:screenNumber, operation:operation!)
+
+                let str = normalize(input: res)
+
+                screenNumber = Double(str)
+
+                label.text = str
+
+                operation = nil
+
+                previousNumber = nil
+
+                performingMath = false
+                
+                return
+            }
+
+            if previousNumber != nil && screenNumber == nil {
+
+                operation = nil
+
+                let str = normalize(input: previousNumber!)
+                let dbl = Double(str)!
+
+                screenNumber = dbl
+                label.text = str
+
+                previousNumber = nil
+                
+                return
+            }
+
             if previousNumber != nil && screenNumber != nil {
                 
                 if (screenNumber! == 0 && operation! == 14) {
-                    //Division by 0
-                    
-                    //Set error
+
                     previousNumber = nil
                     screenNumber = nil
                     operation = nil
@@ -153,17 +257,17 @@ class ViewController: UIViewController {
                     performingMath = false
                     return
                 } else {
-                  
-                    let res = oper(num1:previousNumber, num2:screenNumber, operation:operation!)
-              
-                    let str = normalize(input: res)
-              
-                    let dbl = Double(str)
-              
-                    label.text = str
-                    
-                    previousNumber = nil
 
+                    let res = oper(num1:previousNumber, num2:screenNumber, operation:operation!)
+
+                    let str = normalize(input: res)
+
+                    let dbl = Double(str)
+
+                    label.text = str
+
+                    previousNumber = nil
+   
                     screenNumber = dbl
 
                     operation = nil
@@ -174,6 +278,7 @@ class ViewController: UIViewController {
                 }
             }
         }
+        
     }
     
     func operationToLabel(tag:Int) -> String {
@@ -197,8 +302,8 @@ class ViewController: UIViewController {
             return ""
         }
     }
-            
-        
+    
+    
     func oper(num1: Double?, num2: Double?, operation:Int) -> Double {
         var res: Double = 0
         switch operation {
